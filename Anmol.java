4f00b7bee88bd8826cda3f340fd48f03c053c295
anmolecule94 <141130767+anmolecule94@users.noreply.github.com>
2023-09-18 13:23:06 +0530

    Add files via upload

--- Anmol.java
+++ Anmol.java
@@ -1,7 +1,7 @@
-class Vehicle{   
+class Vehicle1{   
   void run(){System.out.println("Vehicle is running");}  
 }   
-class Bike2 extends Vehicle{  
+class Bike2 extends Vehicle1{  
   void run(){System.out.println("Bike is running safely");}  
   
   public static void main(String args[]){  
